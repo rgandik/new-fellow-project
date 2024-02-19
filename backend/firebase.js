c4721fe11d93e84123e1cd4138002e53fb79905b
Sahil <sahilschowdhury@gmail.com>
2024-02-18 21:30:39 -0600

    added backend auth for firebase

--- backend/firebase.js
+++ backend/firebase.js
@@ -10,4 +10,21 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
+
+const auth = (req, res, next) => {
+  try {
+    const tokenId = req.get("Authorization").split("Bearer ")[1];
+    return admin
+      .auth()
+      .verifyIdToken(tokenId)
+      .then((decoded) => {
+        req.token = decoded;
+        next();
+      })
+      .catch((err) => res.status(401).send(err));
+  } catch (e) {
+    res.status(400).send("Errors");
+  }
+};
+
 module.exports = db;
