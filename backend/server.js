3068fca87d7f38b6fb3a69c7f927b43024e4286e
rgandik <rishi.gandik@gmail.com>
2024-04-02 03:20:08 -0500

    User APIs implemented (create, update, pull)

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ require("dotenv").config();
 const app = express();
 
 const db = require("./firebase");
+db.settings({ ignoreUndefinedProperties: true });
 
 // Middlewares to handle cross-origin requests and to parse the body of incoming requests to JSON
 app.use(cors());
@@ -43,21 +44,112 @@ app.post("/users", async (req, res) => {
   try {
     // Sets the data object to the provided key-value pairs in the request body
     const data = {
-      "uid": req.body.uid,
-      "name": req.body.name ?? "null",
-      "birthday": req.body.birthday ?? "null",
-      "gender": req.body.gender ?? "null",
-      "school": req.body.school ?? "null",
-      "icity": req.body.icity ?? "null",
-      "icompany": req.body.icomponay ?? "null",
-      "irole": req.body.irole ?? "null",
+      "birthday": {
+        "day": Number(req.body.day),
+        "month": Number(req.body.month),
+        "year": Number(req.body.year)
+      },
+      "company": req.body.company,
+      "interests": req.body.interests,
+      "location": {
+        "latitude": Number(req.body.latitude),
+        "longitude": Number(req.body.longitude)
+      },
+      "name": req.body.name,
+      "pfp": req.body.pfp,
+      "school": req.body.school,
+      "uid": req.body.uid
     }
 
-      // Makes the POST request to the database with contents of data object
-      const snapshot = await db.collection("users").doc(data.uid).set(data, { merge: true });
-      
-      // Response with data from POST request and Firestore-generated ID (201 for POST)
-      res.status(201).send(snapshot);
+    // Makes the POST request to the database with contents of data object
+    const snapshot = await db.collection("users").doc(data.uid).set(data, { merge: true });
+
+    // Response with data from POST request and Firestore-generated ID (201 for POST)
+    res.status(201).send("Profile created");
+  } catch(error) {
+    // In case of an error with request, sends an error message instead
+    res.status(500).send(error.message);
+  }
+})
+
+// PUT: Endpoint to update an existing user's profile
+app.put("/users/:user", async (req, res) => {
+  try {
+    const user = req.params.user
+
+    const data = {
+      "birthday": {
+        "day": Number(req.body.day),
+        "month": Number(req.body.month),
+        "year": Number(req.body.year)
+      },
+      "company": req.body.company,
+      "interests": req.body.interests,
+      "location": {
+        "latitude": Number(req.body.latitude),
+        "longitude": Number(req.body.longitude)
+      },
+      "name": req.body.name,
+      "pfp": req.body.pfp,
+      "school": req.body.school
+    }
+
+    // Makes the PUT request to the database with contents of data object
+    const snapshot = await db.collection("users").doc(user).set(data, { merge: true });
+
+    // Response with data from POST request and Firestore-generated ID (201 for POST)
+    res.status(201).send("Profile updated");
+  } catch(error) {
+    // In case of an error with request, sends an error message instead
+    res.status(500).send(error.message);
+  }
+})
+
+// GET: Endpoint to pull a specific user's profile
+app.get("/users/:user", async (req, res) => {
+  try {
+    // Sets the user variable to the query parameter provided in the request URL
+    const user = req.params.user
+
+    // Fetching profile data from "users" for a specific user
+    const snapshot = await db.collection("users").doc(user).get();
+
+    // Response sent with the data recorded in tasks array (200 for GET)
+    res.status(200).send(snapshot.data());
+  }  catch (error) {
+    // In case of an error with request, sends an error message instead
+    res.status(500).send(error.message);
+  }
+})
+
+// POST: Endpoint to create a new group
+app.post("/groups", async (req, res) => {
+  try {
+    // Sets the data object to the provided key-value pairs in the request body
+    const data = {
+      "activity": req.body.activity,
+      "categories": req.body.categories,
+      "description": req.body.description,
+      "location": {
+        "latitude": Number(req.body.latitude),
+        "longitude": Number(req.body.longitude)
+      },
+      "time": {
+        "day": Number(req.body.day),
+        "month": Number(req.body.month),
+        "year": Number(req.body.year),
+        "hour": Number(req.body.hour),
+        "minute": Number(req.body.minute)
+      },
+      "size": Number(req.body.size),
+      "users": req.body.users
+    }  
+  
+    // Makes the POST request to the database with the contents of the data object
+    const snapshot = await db.collection("groups").doc().set(data, { merge: true });
+
+    // Reponse with data from POST request and Firestore-generated ID (201 for POST)
+    res.status(201).send("Circle created");
   } catch(error) {
     // In case of an error with request, sends an error message instead
     res.status(500).send(error.message);
