8613ab133e456eadc2884e7b37210410f9c03665
Sahil <sahilschowdhury@gmail.com>
2024-04-21 17:53:40 -0500

    made it so onboarding input pages don't overflow when typing on keyboard

--- frontend/lib/pages/onboarding/onboarding_achievements.dart
+++ frontend/lib/pages/onboarding/onboarding_achievements.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:frontend/constants.dart';
@@ -23,166 +25,178 @@ class _OnboardingAchievementsState extends State<OnboardingAchievements> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Expanded(
+      body: SingleChildScrollView(
+        // physics: NeverScrollableScrollPhysics(),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height,
+            minWidth: MediaQuery.of(context).size.width,
+          ),
+          child: IntrinsicHeight(
             child: Column(
               children: [
-                const SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: SvgPicture.asset(
-                    'assets/icons/progress2.svg',
-                    height: 100,
-                    width: 20,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: RichText(
-                    text: const TextSpan(
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ), // Default style
-                      children: [
-                        TextSpan(
-                          text: "Share Your",
-                        ),
-                        TextSpan(
-                          text: " Achievements!",
-                          style: TextStyle(color: Constants.myOrange),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 30),
-                  child: Text(
-                    'This is to connect to people who are similar to you!',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 40),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                Expanded(
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        'Internship Company Name',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
+                      const SizedBox(height: 20),
+                      Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: SvgPicture.asset(
+                          'assets/icons/progress2.svg',
+                          height: 100,
+                          width: 20,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30),
+                        child: RichText(
+                          text: const TextSpan(
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ), // Default style
+                            children: [
+                              TextSpan(
+                                text: "Share Your",
+                              ),
+                              TextSpan(
+                                text: " Achievements!",
+                                style: TextStyle(color: Constants.myOrange),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                       const SizedBox(height: 10),
-                      userInputField(
-                        controller: companyController,
-                        error: companyError,
-                        errorMessage: 'Please enter company name',
-                      ), // Assuming this is your custom text input widget
-                      const SizedBox(height: 30),
-                      Text(
-                        'School Name',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 30),
+                        child: Text(
+                          'This is to connect to people who are similar to you!',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
                       ),
-                      const SizedBox(height: 10),
-                      userInputField(
-                        controller: schoolController,
-                        error: schoolError,
-                        errorMessage: 'Please enter school name',
-                      ), // Assuming this is your custom text input widget
+                      const SizedBox(height: 40),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Internship Company Name',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            userInputField(
+                              controller: companyController,
+                              error: companyError,
+                              errorMessage: 'Please enter company name',
+                            ), // Assuming this is your custom text input widget
+                            const SizedBox(height: 30),
+                            Text(
+                              'School Name',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            userInputField(
+                              controller: schoolController,
+                              error: schoolError,
+                              errorMessage: 'Please enter school name',
+                            ), // Assuming this is your custom text input widget
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // Add the buttons row here
+                // const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          // Handle left button press
+                          print('Left button pressed');
+                          Navigator.of(context).pop();
+                          // print the value in onboarding provider
+                          // Consumer<Onboarding_Provider>(
+                          //   builder: (context, provider, child) {
+                          //     print("Location: ");
+                          //     // print(provider.locationController.text);
+                          //     return Container();
+                          //     // return Container();
+                          //   },
+                          // );
+                          final provider = Provider.of<Onboarding_Provider>(context, listen: false);
+                          print("Location: ");
+                          print(provider.locationController.text);
+                        },
+                        child: SvgPicture.asset(
+                          'assets/icons/Back Arrow.svg',
+                          height: 65, // Adjust the height as neededd
+                        ),
+                      ),
+                      const Spacer(),
+                      GestureDetector(
+                        onTap: () {
+                          // Handle right button press
+                          print('Right button pressed');
+                          if (companyController.text.isEmpty) {
+                            setState(() {
+                              companyError = true;
+                            });
+                          } else {
+                            setState(() {
+                              companyError = false;
+                            });
+                          }
+                          if (schoolController.text.isEmpty) {
+                            setState(() {
+                              schoolError = true;
+                            });
+                          } else {
+                            setState(() {
+                              schoolError = false;
+                            });
+                          }
+                          if (!companyError && !schoolError) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => const OnboardingBirthday()),
+                            );
+                            context.read<Onboarding_Provider>().updateCompany(companyController.text);
+                            context.read<Onboarding_Provider>().updateSchool(schoolController.text);
+                          }
+                        },
+                        child: SvgPicture.asset(
+                          'assets/icons/Front Arrow.svg',
+                          height: 65, // Adjust the height as needed
+                          // width: , // Adjust the width as needed
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ],
             ),
           ),
-          // Add the buttons row here
-          // const SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-            child: Row(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    // Handle left button press
-                    print('Left button pressed');
-                    Navigator.of(context).pop();
-                    // print the value in onboarding provider
-                    // Consumer<Onboarding_Provider>(
-                    //   builder: (context, provider, child) {
-                    //     print("Location: ");
-                    //     // print(provider.locationController.text);
-                    //     return Container();
-                    //     // return Container();
-                    //   },
-                    // );
-                    final provider = Provider.of<Onboarding_Provider>(context, listen: false);
-                    print("Location: ");
-                    print(provider.locationController.text);
-                  },
-                  child: SvgPicture.asset(
-                    'assets/icons/Back Arrow.svg',
-                    height: 65, // Adjust the height as neededd
-                  ),
-                ),
-                const Spacer(),
-                GestureDetector(
-                  onTap: () {
-                    // Handle right button press
-                    print('Right button pressed');
-                    if (companyController.text.isEmpty) {
-                      setState(() {
-                        companyError = true;
-                      });
-                    } else {
-                      setState(() {
-                        companyError = false;
-                      });
-                    }
-                    if (schoolController.text.isEmpty) {
-                      setState(() {
-                        schoolError = true;
-                      });
-                    } else {
-                      setState(() {
-                        schoolError = false;
-                      });
-                    }
-                    if (!companyError && !schoolError) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const OnboardingBirthday()),
-                      );
-                      context.read<Onboarding_Provider>().updateCompany(companyController.text);
-                      context.read<Onboarding_Provider>().updateSchool(schoolController.text);
-                    }
-                  },
-                  child: SvgPicture.asset(
-                    'assets/icons/Front Arrow.svg',
-                    height: 65, // Adjust the height as needed
-                    // width: , // Adjust the width as needed
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
--- frontend/lib/pages/onboarding/onboarding_location.dart
+++ frontend/lib/pages/onboarding/onboarding_location.dart
@@ -20,124 +20,136 @@ class _OnboardingLocationState extends State<OnboardingLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Expanded(
+      body: SingleChildScrollView(
+        // physics: NeverScrollableScrollPhysics(),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height,
+            minWidth: MediaQuery.of(context).size.width,
+          ),
+          child: IntrinsicHeight(
             child: Column(
               children: [
-                const SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: SvgPicture.asset(
-                    'assets/icons/progress1.svg',
-                    height: 100,
-                    width: 20,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: RichText(
-                    text: const TextSpan(
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ), // Default style
-                      children: [
-                        TextSpan(
-                          text: "Where Are You ",
+                Expanded(
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: SvgPicture.asset(
+                          'assets/icons/progress1.svg',
+                          height: 100,
+                          width: 20,
                         ),
-                        TextSpan(
-                          text: "Located",
-                          style: TextStyle(color: Constants.myOrange),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30),
+                        child: RichText(
+                          text: const TextSpan(
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ), // Default style
+                            children: [
+                              TextSpan(
+                                text: "Where Are You ",
+                              ),
+                              TextSpan(
+                                text: "Located",
+                                style: TextStyle(color: Constants.myOrange),
+                              ),
+                              TextSpan(
+                                text: "?",
+                              ),
+                            ],
+                          ),
                         ),
-                        TextSpan(
-                          text: "?",
+                      ),
+                      const SizedBox(height: 10),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 30),
+                        child: Text(
+                          'Type in the area you will be located during your internship to help us find activities close to you!',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
-                      ],
-                    ),
+                      ),
+                      const SizedBox(height: 40),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30),
+                        child: userInputField(
+                          controller: locationController,
+                          error: showError,
+                          errorMessage: showError ? 'Location is required' : null,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      // Adjust the spacing between the text field and the image
+                      SvgPicture.asset(
+                        'assets/icons/mapimage.svg',
+                        // Replace 'WhereAreYouLocatedImage.svg' with your SVG image path
+                        height: 300, // Adjust the height of the image as needed
+                        width: 400, // Make the image take the full width
+                        fit: BoxFit.cover, // Adjust the fit of the image as needed
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 10),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 30),
-                  child: Text(
-                    'Type in the area you will be located during your internship to help us find activities close to you!',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 40),
+                // Add the buttons row here
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: userInputField(
-                    controller: locationController,
-                    error: showError,
-                    errorMessage: showError ? 'Location is required' : null,
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          // Handle left button press
+                          print('Left button pressed');
+                          Navigator.of(context).pop();
+                        },
+                        child: SvgPicture.asset(
+                          'assets/icons/Back Arrow.svg',
+                          height: 65, // Adjust the height as needed
+                        ),
+                      ),
+                      const Spacer(),
+                      GestureDetector(
+                        onTap: () {
+                          // Handle right button press
+                          if (locationController.text.isNotEmpty) {
+                            setState(() {
+                              showError = false;
+                            });
+                            print('Right button pressed');
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => OnboardingAchievements()),
+                            );
+                            context.read<Onboarding_Provider>().updateLocation(locationController.text);
+                          } else {
+                            setState(() {
+                              showError = true;
+                            });
+                          }
+                        },
+                        child: SvgPicture.asset(
+                          'assets/icons/Front Arrow.svg',
+                          height: 65, // Adjust the height as needed
+                          // width: , // Adjust the width as needed
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(height: 20),
-                // Adjust the spacing between the text field and the image
-                SvgPicture.asset(
-                  'assets/icons/mapimage.svg',
-                  // Replace 'WhereAreYouLocatedImage.svg' with your SVG image path
-                  height: 300, // Adjust the height of the image as needed
-                  width: 400, // Make the image take the full width
-                  fit: BoxFit.cover, // Adjust the fit of the image as needed
                 ),
               ],
             ),
           ),
-          // Add the buttons row here
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Row(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    // Handle left button press
-                    print('Left button pressed');
-                    Navigator.of(context).pop();
-                  },
-                  child: SvgPicture.asset(
-                    'assets/icons/Back Arrow.svg',
-                    height: 65, // Adjust the height as needed
-                  ),
-                ),
-                const Spacer(),
-                GestureDetector(
-                  onTap: () {
-                    // Handle right button press
-                    if (locationController.text.isNotEmpty) {
-                      setState(() {
-                        showError = false;
-                      });
-                      print('Right button pressed');
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => OnboardingAchievements()),
-                      );
-                      context.read<Onboarding_Provider>().updateLocation(locationController.text);
-                    } else {
-                      setState(() {
-                        showError = true;
-                      });
-                    }
-                  },
-                  child: SvgPicture.asset(
-                    'assets/icons/Front Arrow.svg',
-                    height: 65, // Adjust the height as needed
-                    // width: , // Adjust the width as needed
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
