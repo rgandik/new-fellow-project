b55cb354fd2c9cd0f239b1e7587cf707ce2cd640
rgandik <rishi.gandik@gmail.com>
2024-04-21 21:46:31 -0500

    Profile generation is functional

--- frontend/lib/pages/onboarding/onboarding_interests.dart
+++ frontend/lib/pages/onboarding/onboarding_interests.dart
@@ -5,6 +5,7 @@ import 'package:frontend/pages/authentication_screen.dart';
 import 'package:frontend/widgets/myEntryField.dart';
 import 'package:provider/provider.dart';
 import 'package:frontend/providers/onboarding_provider.dart';
+import '../../providers/auth_provider.dart';
 import 'onboarding_done.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
@@ -46,15 +47,33 @@ class _OnboardingInterestsState extends State<OnboardingInterests> {
     });
   }
 
-  Future<void> createUserProfile(String location, String company, String school, ) async {
-    var url = 'https://new-fellow-project.vercel.app/gusers';
+  Future<bool> createUserProfile(/*String location,*/ String company, String school, int day, int month, int year, List<String> interests, String uid) async {
+    var url = 'https://new-fellow-project.vercel.app/users';
 
     final response = await http.post(
       Uri.parse(url),
-      body: {
-
-      }
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        //'location': location,
+        'company': company,
+        'school': school,
+        'day': day,
+        'month': month,
+        'year': year,
+        'interests': interests,
+        'uid': uid
+      })
     );
+    if (response.statusCode == 201) {
+      print(response.body);
+      return true;
+    } else {
+      print("Error creating user profile");
+      print(response.body);
+      return false;
+    }
   }
 
   @override
@@ -78,14 +97,14 @@ class _OnboardingInterestsState extends State<OnboardingInterests> {
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 30),
                   child: RichText(
-                    text: const TextSpan(
-                      style: TextStyle(
+                    text: TextSpan(
+                      style: const TextStyle(
                         fontSize: 40,
                         fontWeight: FontWeight.bold,
                         color: Colors.black,
                       ), // Default style
                       children: [
-                        TextSpan(
+                        const TextSpan(
                           text: "Select Your",
                         ),
                         TextSpan(
@@ -110,8 +129,8 @@ class _OnboardingInterestsState extends State<OnboardingInterests> {
                   ),
                 ),
                 const SizedBox(height: 10), // Added for spacing
-                Padding(
-                  padding: const EdgeInsets.only(left: 30),
+                const Padding(
+                  padding: EdgeInsets.only(left: 30),
                   // Adjust the padding as needed
                   child: Align(
                     alignment: Alignment.centerLeft,
@@ -144,7 +163,7 @@ class _OnboardingInterestsState extends State<OnboardingInterests> {
                                       width: 20),
                                 ),
                                 Text(label,
-                                    style: TextStyle(
+                                    style: const TextStyle(
                                       fontSize: 17,
                                       color: Colors.black,
                                       fontWeight: FontWeight.w600,
@@ -195,8 +214,20 @@ class _OnboardingInterestsState extends State<OnboardingInterests> {
                         .read<Onboarding_Provider>()
                         .updateSelectedInterests(selectedInterests);
                     context.read<Onboarding_Provider>().finishOnboarding();
+                    AuthProvider authProvider = Provider.of<AuthProvider>(context,
+                        listen: false);
                     final provider = Provider.of<Onboarding_Provider>(context,
                         listen: false);
+                    createUserProfile(
+                      //provider.location as String,
+                      provider.company()!,
+                      provider.school(),
+                      provider.day()!,
+                      provider.month()!,
+                      provider.year()!,
+                      selectedInterests,
+                      authProvider.uid()!
+                    );
                   },
                   child: SvgPicture.asset(
                     'assets/icons/Front Arrow.svg',
